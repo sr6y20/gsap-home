8adc45c488685b39f23b85d62a9c5e3cbfe05b15
sr6y20 <1836165455@qq.com>
2025-04-10 14:58:16 +0800

    neon背景

--- src/pages/Home/components/NeonRoom.vue
+++ src/pages/Home/components/NeonRoom.vue
@@ -114,6 +114,18 @@ onMounted(() => {
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
 
+@property --hue1 {
+    syntax: "<angle>";
+    inherits: false;
+    initial-value: 0deg;
+}
+
+@property --hue2 {
+    syntax: "<angle>";
+    inherits: false;
+    initial-value: 0deg;
+}
+
 .final {
     position: relative;
     display: flex;
@@ -121,7 +133,32 @@ onMounted(() => {
     align-items: center;
     padding: 10px;
     height: 100vh;
-    background: linear-gradient(45deg, #0e2991, transparent);
+    background-image: linear-gradient(in oklch longer hue to right,
+            oklch(0.93 0.08 var(--hue1) / 50%),
+            oklch(0.93 0.08 var(--hue2) / 50%)),
+        linear-gradient(in oklch longer hue to bottom,
+            oklch(0.93 0.08 var(--hue1) / 50%),
+            oklch(0.93 0.08 var(--hue2) / 50%));
+
+    background-size: 100% 100%;
+    color: PaleVioletRed;
+
+    animation-name: anim_bg;
+    animation-duration: 5s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+
+    @keyframes anim_bg {
+        0% {
+            --hue1: 0deg;
+            --hue2: 300deg;
+        }
+
+        100% {
+            --hue1: 360deg;
+            --hue2: 660deg;
+        }
+    }
 
     .card,
     .content {
